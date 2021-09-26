8acc89b091725b5961c0767c690f116e2185f887
snaker938 <jmendel123@gmail.com>
2021-09-26 23:45:07 +0100

    Added functionality to the forms
    Added python code to make the html signup forms functional. The user is now stored to a database which is created, and can log in. The user can then logout once logged in.

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,77 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask.json.tag import PassDict
+from .models import User
+from website import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    return  render_template("login.html", text="Testing")
+    
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect password, try again.", category="error")
+        else:
+            flash("Email does not exist", category="error")
+
+    return  render_template("login.html", user=current_user)
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for("auth.login"))
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def signup():
-    return render_template("sign_up.html")
+
+
+    def check_password(password):
+        if (any(x.isupper() for x in password) and any(x.islower() for x in password) and any(x.isdigit() for x in password) and (len(password) >= 6)):
+            return True
+        else:
+            return False
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        first_name = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash("Email already exists!", category="error")
+        elif len(email) < 4:
+            flash("Email must be more than 4 characters long!", category="error")
+        elif len(first_name) < 2:
+            flash("Your first name must be more than 2 characters long!", category="error")
+        elif password1 != password2:
+            flash("Your passwords do not match!", category="error")
+        elif not(check_password(password1)):
+            flash("Your password must contain at least 1 capital letter, one lowercase letter, one digit and it must be bigger than 6 characters long!", category="error")
+        else:
+            # add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash("Account created!", category="success")
+            return redirect(url_for("views.home"))
+    
+    
+    
+    return render_template("sign_up.html", user=current_user)
+
 
