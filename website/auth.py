c4c391cbbfbdf57c0ac34629cc17e7dfeff8d8f5
snaker938 <jmendel123@gmail.com>
2021-09-26 21:58:19 +0100

    Created Login and Signup html forms
    I created a pages where the users can login and signup. No python is operating on the values- it is purely html for these forms currently.

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
+    return  render_template("login.html", text="Testing")
 
 @auth.route("/logout")
 def logout():
@@ -12,5 +12,5 @@ def logout():
 
 @auth.route("/sign-up")
 def signup():
-    return "<p>Sign-up</p>"
+    return render_template("sign_up.html")
 
